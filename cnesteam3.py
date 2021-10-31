8b04e99b2efe2a8bd59aaba863e4bf57f81246fd
Hugo Souza <59892459+dezugin@users.noreply.github.com>
2021-10-31 16:41:56 +0000

    cnes final

--- cnesteam3.py
+++ cnesteam3.py
@@ -9,14 +9,14 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException, WebDriverException, TimeoutException 
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException, WebDriverException, TimeoutException
 import time
 import pandas as pd
 import traceback
 
 #load CNES file, webdriver and current count of CNES number
 with open('restart.txt','r') as c:
-  count = int(c.read())
+  count = int(c.read())-1
 centrosDeSaude = pd.read_csv('CentrosDeSaudeBHComCPFTraduzido.csv')
 keys = centrosDeSaude['CNES'].unique().astype(int)
 conditionalKey = 0
@@ -24,7 +24,7 @@ driver = webdriver.Firefox()
 
 #loop to iterate through entire CNES list
 while count<len(keys):
-  
+
   try:
 
     #values of consecutive CNES number to begin from, month and year
@@ -35,8 +35,9 @@ while count<len(keys):
     #main loop, iterate through keys
     for key in keys:
       if count<=continueCount:
+        print(count," + ",keys[count]," + ",continueCount)
         count = count +1
-        continue
+      key = keys[count]
       cnes = f'{key:07d}'
       conditionForReloading = True
 
@@ -51,21 +52,21 @@ while count<len(keys):
           print("Website loading not successful")
 
       out = " Inicio: "+cnes+" Count: "+str(count)
-      with open('out.txt', 'w') as f:
+      with open('out.txt', 'a') as f:
         print(out, file=f)
       print(out)
       driver.find_element(By.CSS_SELECTOR, "li:nth-child(11) span").click()
       out = " Atual: "+cnes+" Count: "+str(count)
-      with open('out.txt', 'w') as f:
+      with open('out.txt', 'a') as f:
         print(out, file=f)
       print(out)
-      
+
       # downloading current year's data
       for i in range(1,11):
-      
+
         if count<=continueCount:
           continue
-      
+
         try:
           driver.find_element(By.CSS_SELECTOR, f'.ng-scope:nth-child({i}) > .text-center .glyphicon').click()
           rows = len (driver.find_elements_by_xpath("//*[@id='listaProfEquipes']/tbody/tr"))
@@ -75,20 +76,21 @@ while count<len(keys):
           aftertr_XPath = "]"
           time.sleep(0.3)
           text = driver.find_element_by_xpath("//*[@id='listaProfEquipes']/thead/tr[1]/th").text
-          
+
           if len(text)>0:
             title = text
             dataframelist = []
-          
+
             for t_row in range(2, (rows + 1)):
               dataframerow = []
               dataframerow.append(title)
               for t_column in range(1, (columns + 1)):
                 FinalXPath = before_XPath + str(t_row) + aftertd_XPath + str(t_column) + aftertr_XPath
-                cell_text = driver.find_element_by_xpath(FinalXPath).text
+                cell_text = driver.find_element_by_xpath(FinalXPath).get_attribute("innerHTML")
                 dataframerow.append(cell_text)
+                
               dataframerow.append("10/21")
-              dataframerow.append(cnes)  
+              dataframerow.append(cnes)
               dataframelist.append(dataframerow)
 
             df = pd.DataFrame(dataframelist, columns=['Equipe','Nome','CNS','CBO','Atividade','Equipe Minima','Hospitalar','Ambulatorial','Outras','Diferenciada','Complementar','Data Entrada','Data do arquivo','CNES'])
@@ -107,15 +109,15 @@ while count<len(keys):
           with open('outcnes.txt', 'a') as f:
             f.write(outatual)
           continue
-      
+
       #downloading current year's data, second tab
 
       for i in range(1,11):
 
         if count<continueCount+1:
           continue
-        
-        try:  
+
+        try:
           driver.find_element(By.LINK_TEXT, "2").click()
           driver.find_element(By.CSS_SELECTOR, f'.ng-scope:nth-child({i}) > .text-center .glyphicon').click()
           rows = len (driver.find_elements_by_xpath("//*[@id='listaProfEquipes']/tbody/tr"))
@@ -125,7 +127,7 @@ while count<len(keys):
           aftertr_XPath = "]"
           time.sleep(0.3)
           text = driver.find_element_by_xpath("//*[@id='listaProfEquipes']/thead/tr[1]/th").text
-          
+
           if len(text)>0:
             title = text
             dataframelist = []
@@ -135,10 +137,10 @@ while count<len(keys):
               dataframerow.append(title)
               for t_column in range(1, (columns + 1)):
                 FinalXPath = before_XPath + str(t_row) + aftertd_XPath + str(t_column) + aftertr_XPath
-                cell_text = driver.find_element_by_xpath(FinalXPath).text
+                cell_text = driver.find_element_by_xpath(FinalXPath).get_attribute("innerHTML")
                 dataframerow.append(cell_text)
               dataframerow.append("10/21")
-              dataframerow.append(cnes)  
+              dataframerow.append(cnes)
               dataframelist.append(dataframerow)
 
             df = pd.DataFrame(dataframelist, columns=['Equipe','Nome','CNS','CBO','Atividade','Equipe Minima','Hospitalar','Ambulatorial','Outras','Diferenciada','Complementar','Data Entrada','Data do arquivo','CNES'])
@@ -166,13 +168,13 @@ while count<len(keys):
 
           if (count==continueCount and ano>continueYear) or (count ==continueCount and (ano >= continueYear and mes >continueMonth)):
             continue
-          
+
           try:
             time.sleep(0.5)
             driver.find_element_by_xpath("//*[@id='estabContent']/header/nav/form/div/select").click()
             driver.find_element(By.XPATH, f"//option[. = '{data}']").click()
             out = data+" Inicio: "+cnes+" Count: "+str(count)
-            with open('out.txt', 'w') as f:
+            with open('out.txt', 'a') as f:
               print(out, file=f)
             print(out)
 
@@ -192,22 +194,22 @@ while count<len(keys):
                 if len(text)>0:
                   title = text
                   dataframelist = []
-                
+
                   for t_row in range(2, (rows + 1)):
                     dataframerow = []
                     dataframerow.append(title)
                     for t_column in range(1, (columns + 1)):
                       FinalXPath = before_XPath + str(t_row) + aftertd_XPath + str(t_column) + aftertr_XPath
-                      cell_text = driver.find_element_by_xpath(FinalXPath).text
+                      cell_text = driver.find_element_by_xpath(FinalXPath).get_attribute("innerHTML")
                       dataframerow.append(cell_text)
                     dataframerow.append(data)
-                    dataframerow.append(cnes)  
+                    dataframerow.append(cnes)
                     dataframelist.append(dataframerow)
 
                   df = pd.DataFrame(dataframelist, columns=['Equipe','Nome','CNS','CBO','Atividade','Equipe Minima','Hospitalar','Ambulatorial','Outras','Diferenciada','Complementar','Data Entrada','Data do arquivo','CNES'])
                   title = title.replace("/", "-")
                   df.to_csv(f'/home/dez/selenium/data/{cnes}-{title}{data2}.csv')
-                
+
                 time.sleep(0.3)
                 ActionChains(driver).send_keys(Keys.ESCAPE).perform()
                 time.sleep(0.5)
@@ -218,9 +220,9 @@ while count<len(keys):
                 continue
 
             for i in range(1,11):
-              
+
               try:
-                ActionChains(driver).send_keys(Keys.ESCAPE).perform()  
+                ActionChains(driver).send_keys(Keys.ESCAPE).perform()
                 driver.find_element(By.LINK_TEXT, "2").click()
                 driver.find_element(By.CSS_SELECTOR, f'.ng-scope:nth-child({i}) > .text-center .glyphicon').click()
                 rows = len (driver.find_elements_by_xpath("//*[@id='listaProfEquipes']/tbody/tr"))
@@ -229,26 +231,26 @@ while count<len(keys):
                 aftertd_XPath = "]/td["
                 aftertr_XPath = "]"
                 time.sleep(0.3)
-              
+
                 text = driver.find_element_by_xpath("//*[@id='listaProfEquipes']/thead/tr[1]/th").text
                 if len(text)>0:
                   title = text
                   dataframelist = []
-              
+
                   for t_row in range(2, (rows + 1)):
                     dataframerow = []
                     dataframerow.append(title)
                     for t_column in range(1, (columns + 1)):
                       FinalXPath = before_XPath + str(t_row) + aftertd_XPath + str(t_column) + aftertr_XPath
-                      cell_text = driver.find_element_by_xpath(FinalXPath).text
+                      cell_text = driver.find_element_by_xpath(FinalXPath).get_attribute("innerHTML")
                       dataframerow.append(cell_text)
                     dataframerow.append(data)
-                    dataframerow.append(cnes)  
+                    dataframerow.append(cnes)
                     dataframelist.append(dataframerow)
                   df = pd.DataFrame(dataframelist, columns=['Equipe','Nome','CNS','CBO','Atividade','Equipe Minima','Hospitalar','Ambulatorial','Outras','Diferenciada','Complementar','Data Entrada','Data do arquivo','CNES'])
                   title = title.replace("/", "-")
                   df.to_csv(f'/home/dez/selenium/data/{cnes}-{title}{data2}.csv')
-              
+
                 time.sleep(0.3)
                 ActionChains(driver).send_keys(Keys.ESCAPE).perform()
                 time.sleep(0.5)
@@ -271,18 +273,19 @@ while count<len(keys):
             with open('outcnes.txt', 'a') as f:
               f.write(str(count)+outatual)
             continue
-        
+
         out = str(count)+data+" Fim:"+cnes+" Count: "+str(count)
-        with open('out.txt', 'w') as f:
+        with open('out.txt', 'a') as f:
           print(out, file=f)
         print(out)
-      
+
       out = str(count)+"Terminando: "+cnes+" Count: "+str(count)
-      with open('out.txt', 'w') as f:
+      with open('out.txt', 'a') as f:
         print(out, file=f)
       print(out)
       count = count + 1
-
+      with open('restart.txt','w') as cou:
+        print(count,file=cou)
     with open('restart.txt', 'w') as f:
       countln = str(count)
       print(countln, file=f)
